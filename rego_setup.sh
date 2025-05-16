103535cc630d3ee71ffa09092b2d485915800b75
GabeCasciano <gabecasciano@gmail.com>
2025-05-16 10:07:50 -0400

    regolith setup script ready and working

--- rego_setup.sh
+++ rego_setup.sh
@@ -3,6 +3,14 @@ set -euo pipefail
 
 echo "[*] Installing Regolith Desktop..."
 
+echo " Adding PPA"
+wget -qO - https://archive.regolith-desktop.com/regolith.key | \
+gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+https://archive.regolith-desktop.com/ubuntu/stable noble v3.2" | \
+sudo tee /etc/apt/sources.list.d/regolith.list
+
 # Ensure prerequisites
 sudo apt update -qq
 sudo apt install -y -qq software-properties-common curl
